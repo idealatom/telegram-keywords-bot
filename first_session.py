9d89236f0681ae72bec6d80fb805653622fca6f7
DS-jr <dmitry.k.sokolov@gmail.com>
2022-12-28 19:00:17 +0400

    updated first_session.py file

--- first_session.py
+++ first_session.py
@@ -1,16 +1,16 @@
 from configparser import ConfigParser
 
-def create_configini_file():  # Is this a good naming for the function?  ***Also: check bot.py file
+def create_configini_file():
     config = ConfigParser()
-    econfig.read('config.ini')  # ? Can config.ini file be read, if it has NOT been created yet?
+    config.read('config.ini')
 
-    if not config.has_section('pyrogram'):  # ? How does the script understand that config.ini file should be opened?
-        config.add_section('pyrogram')  # ? Can 'pyrogram' section be added, if config.ini file has NOT been created yet?
+    if not config.has_section('pyrogram'):
+        config.add_section('pyrogram')
         with open('config.ini', 'w') as configfile:
             config.write(configfile)
 
-    api_id = config.get('pyrogram', 'api_id', fallback=None)  # ? Is this line vital now? If yes - is it in the right place?
-    api_hash = config.get('pyrogram', 'api_hash', fallback=None)  # ? Is this line vital now? If yes - is it in the right place?
+    api_id = config.get('pyrogram', 'api_id', fallback=None)
+    api_hash = config.get('pyrogram', 'api_hash', fallback=None)
 
     if not api_id:
         inputed_api_id = input("Paste your Telegram App's `api_id' here: ")
@@ -25,25 +25,3 @@ def create_configini_file():  # Is this a good naming for the function?  ***Also
         config.set('pyrogram', 'api_hash', inputed_api_hash)
         with open('config.ini', 'w') as configfile:
             config.write(configfile)
-
-
-
-# configini_file()   #  for testing
-    # 0. Check if config.ini file exists, if it's not - create it.
-    # with open('config.ini', 'a') as config_ini_file:
-    #     if not api_id and api_hash in config_ini_file:
-    #         api_id_input = input("Please, paste your Telegram App's `api_id' here (get it from 'App configuration' at https://my.telegram.org/apps"))
-    #         api_id_hash = input("Please, paste your Telegram App's `api_hash' here (get it from 'App configuration' at https://my.telegram.org/apps"))
-
-
-    # def config_set_and_save(section, param_name, param_value, skip_set=False):
-    #     if (not skip_set):
-    #         config.set(section, param_name, param_value)
-    #     with open('config.ini', 'w') as configfile:
-    #         config.write(configfile)
-
-
-    # if api_id:
-    #     print('aCt!')
-        # api_id = input("Please, paste your Telegram App's `api_id' here (get it from 'App configuration' at https://my.telegram.org/apps")
-#        hash_id = input("Please, paste your Telegram App's `api_hash' here (get it from 'App configuration' at https://my.telegram.org/apps")
